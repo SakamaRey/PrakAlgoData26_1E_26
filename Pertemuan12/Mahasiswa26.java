fbea46dc49a0ff5b52e1b8bfac6970e21c39db6c
Resya Fajar Putra P <112920059+SakamaRey@users.noreply.github.com>
2026-05-06 23:03:22 +0700

    Pertemuan12_Praktikum 1

---
+++ Pertemuan12/Mahasiswa26.java
@@ -0,0 +1,22 @@
+public class Mahasiswa26 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa26(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    void tampilInformasi() {
+        System.out.println(
+                nama + "\t" +
+                nim + "\t" +
+                kelas + "\t" +
+                ipk
+        );
+    }
+}
