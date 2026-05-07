93c31255baf954158a6b34855f897b365309d0e1
Resya Fajar Putra P <112920059+SakamaRey@users.noreply.github.com>
2026-05-07 08:06:25 +0700

    Pertemuan12_Praktikum2

--- Pertemuan12/SLLMain26.java
+++ Pertemuan12/SLLMain26.java
@@ -1,49 +1,63 @@
 
-import java.util.Scanner;
 
 public class SLLMain26 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         SingleLinkedList26 sll = new SingleLinkedList26();
         
-        System.out.print("Masukkan jumlah mahasiswa : ");
-        int jumlah = sc.nextInt();
-        sc.nextLine();
+        // System.out.print("Masukkan jumlah mahasiswa : ");
+        // int jumlah = sc.nextInt();
+        // sc.nextLine();
 
-        for (int i = 0; i < jumlah; i++) {
+        // for (int i = 0; i < jumlah; i++) {
 
-            System.out.println("\nData Mahasiswa ke-" + (i + 1));
-            System.out.print("NIM   : ");
-            String nim = sc.nextLine();
-            System.out.print("Nama  : ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas : ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK   : ");
-            double ipk = sc.nextDouble();
-            sc.nextLine();
+        //     System.out.println("\nData Mahasiswa ke-" + (i + 1));
+        //     System.out.print("NIM   : ");
+        //     String nim = sc.nextLine();
+        //     System.out.print("Nama  : ");
+        //     String nama = sc.nextLine();
+        //     System.out.print("Kelas : ");
+        //     String kelas = sc.nextLine();
+        //     System.out.print("IPK   : ");
+        //     double ipk = sc.nextDouble();
+        //     sc.nextLine();
 
-            Mahasiswa26 mhs = new Mahasiswa26(nim,nama,kelas,ipk);
-            sll.addLast(mhs);
-        }
+        //     Mahasiswa26 mhs = new Mahasiswa26(nim,nama,kelas,ipk);
+        //     sll.addLast(mhs);
+        // }
 
         System.out.println("\nData Linked List");
         sll.print();
-    }
-        // Mahasiswa26 mhs1 = new Mahasiswa26("21212203", "Alvaro", "1A", 4.0);
-        // Mahasiswa26 mhs2 = new Mahasiswa26("23212201", "Bamon", "2B", 3.0);
-        // Mahasiswa26 mhs3 = new Mahasiswa26("22212202", "Cintia", "3C", 3.5);
-        // Mahasiswa26 mhs4 = new Mahasiswa26("21212203", "Dirga", "4D", 3.6);
+    
+        Mahasiswa26 mhs1 = new Mahasiswa26("21212203", "Alvaro", "1A", 4.0);
+        Mahasiswa26 mhs2 = new Mahasiswa26("23212201", "bimon", "2B", 3.0);
+        Mahasiswa26 mhs3 = new Mahasiswa26("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa26 mhs4 = new Mahasiswa26("21212203", "Dirga", "4D", 3.6);
 
         // sll.print();
 
         // sll.addFirst(mhs4);
         // sll.print();
 
-        // sll.addLast(mhs1);
+        sll.addLast(mhs1);
         // sll.print();
 
         // sll.insertAfter("Dirga", mhs3);
         // sll.insertAt(2, mhs2);
         // sll.print();
+
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+        System.out.println("data mahasiswa an Bimon berada pada index : "+ sll.indexOf("Bimon")
+        );
+        System.out.println("");
+        sll.removeFirst();
+        sll.print();
+
+        sll.removeLast();
+        sll.print();
+
+        sll.removeAt(0);
+        sll.print();
     }
+}
