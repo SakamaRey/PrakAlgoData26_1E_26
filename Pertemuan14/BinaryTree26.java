424ffcba8144fc36fcdac22fcd3887403e42b444
Resya Fajar Putra P <112920059+SakamaRey@users.noreply.github.com>
2026-05-26 21:19:19 +0700

    Praktikum1_Jobsheet14

---
+++ Pertemuan14/BinaryTree26.java
@@ -0,0 +1,167 @@
+public class BinaryTree26 {
+    Node26 root;
+
+    public BinaryTree26() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void add(Mahasiswa26 mahasiswa) {
+        Node26 newNode = new Node26(mahasiswa);
+        if (isEmpty()) {
+            root = newNode;
+        } else {
+            Node26 current = root;
+            Node26 parent = null;
+            while (true) { 
+                parent = current;
+                if (mahasiswa.ipk < current.mahasiswa.ipk) {
+                    current = current.left;
+                    if (current == null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                } else {
+                    current = current.right;
+                    if (current == null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    boolean find(double ipk) {
+        boolean result = false;
+        Node26 current = root;
+        while (current != null) { 
+            if (current.mahasiswa.ipk == ipk) {
+                result = true;
+                break;
+            } else if (ipk > current.mahasiswa.ipk) {
+                current = current.right;
+            }else {
+                current = current.left;
+            }
+        }
+        return result;
+    }
+
+    void traversePreOrder(Node26 node) {
+        if (node != null) {
+            node.mahasiswa.tampilInformasi();
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    void traverseInOrder(Node26 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            node.mahasiswa.tampilInformasi();
+            traverseInOrder(node.right);
+        }
+    }
+
+    void traversePostOrder(Node26 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            node.mahasiswa.tampilInformasi();
+        }
+    }
+
+    Node26 getSuccessor(Node26 del) {
+        Node26 successor = del.right;
+        Node26 successorParent = del;
+        while (successor.left != null) { 
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return  successor;
+    }
+
+    void deleted(double ipk) {
+        if (isEmpty()) {
+            System.out.println("Binary tree kosong");
+            return;
+        }
+        // Cari node (current) yang akan dihapus
+        Node26 parent = root;
+        Node26 current = root;
+        boolean isLeftChild = false;
+        while (current != null) { 
+            if (current.mahasiswa.ipk == ipk) {
+                break;
+            } else if (ipk < current.mahasiswa.ipk) {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else if (ipk > current.mahasiswa.ipk) {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+        // penghapusan
+        if (current == null) {
+            System.out.println("Data tidak ditemukan");
+            return;
+        } else {
+            //JIka tidak ada anak (leaf) , maka node dihapus
+            if (current.left == null && current.right == null) {
+                if (current == root) {
+                    root = null;
+            } else {
+                if (isLeftChild) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+                }
+            } else if (current.left == null) {
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) {
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else {
+                Node26 successor = getSuccessor(current);
+                System.out.println("Jika 2 anak, current = ");
+                successor.mahasiswa.tampilInformasi();
+                if (current == root) {
+                    root = successor;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    } else {
+                        parent.right = successor;
+                    }
+                }
+                successor.left = current.left;
+            }
+        }
+    }
+}
